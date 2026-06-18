1769f085ab804071e7ad9e77318c5ad1dcaf8842
c0remusic <rawrnie@gmail.com>
2026-06-19 01:32:09 +0200

    fix(installer): support Inno Setup 6 and 7 in ISCC path detection

--- build_installer.py
+++ build_installer.py
@@ -14,6 +14,8 @@ ISS  = os.path.join(ROOT, "installer", "tuple.iss")
 ISCC_CANDIDATES = [
     r"C:\Program Files (x86)\Inno Setup 6\ISCC.exe",
     r"C:\Program Files\Inno Setup 6\ISCC.exe",
+    r"C:\Program Files (x86)\Inno Setup 7\ISCC.exe",
+    r"C:\Program Files\Inno Setup 7\ISCC.exe",
 ]
 
 def find_iscc():
